fb3b42411878860bf5e0cb4788d8baa5fcca03c7
MatiasMtz <4442@holbertonschool.com>
2022-08-31 23:16:12 +0000

    Poetry Club Door Policy exercise

---
+++ Exercism/Javascritpt/4-PoetryClubDoorPolicy.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+	return line[0];
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export function frontDoorPassword(word) {
+	return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+	var trimmedLine = line.trim()
+	return trimmedLine.charAt(trimmedLine.length - 1);
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export function backDoorPassword(word) {
+	return word[0].toUpperCase() + word.slice(1).toLowerCase() + ', please';
+}
